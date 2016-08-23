7a6b15cd920a105f8ba8beded79bac3eb9c555e5
Danny Dulai <danny@ishiboo.com>
2016-08-22 22:25:17 -0400

    more methods, keep track of zones

--- lib.js
+++ lib.js
@@ -19,6 +19,27 @@ function RoonApiTransport(core) {
 
 RoonApiTransport.services = [ { name: SVCNAME } ];
 
+RoonApiTransport.prototype.mute_all = function(cb) {
+    this.core.moo.send_request(SVCNAME+"/mute_all",
+                               (msg, body) => {
+                                   if (cb)
+                                       cb(msg && msg.name == "Success" ? false : (msg ? msg.name : "NetworkError"));
+                               });
+};
+RoonApiTransport.prototype.unmute_all = function(cb) {
+    this.core.moo.send_request(SVCNAME+"/unmute_all",
+                               (msg, body) => {
+                                   if (cb)
+                                       cb(msg && msg.name == "Success" ? false : (msg ? msg.name : "NetworkError"));
+                               });
+};
+RoonApiTransport.prototype.pause_all = function(cb) {
+    this.core.moo.send_request(SVCNAME+"/pause_all",
+                               (msg, body) => {
+                                   if (cb)
+                                       cb(msg && msg.name == "Success" ? false : (msg ? msg.name : "NetworkError"));
+                               });
+};
 RoonApiTransport.prototype.mute = function(z, how, cb) {
     this.core.moo.send_request(SVCNAME+"/mute",
                                {
@@ -122,6 +143,38 @@ RoonApiTransport.prototype.get_outputs = function(cb) {
 };
 
 RoonApiTransport.prototype.subscribe_outputs  = function(cb) { this.core.moo._subscribe_helper(SVCNAME, "outputs", cb); }
-RoonApiTransport.prototype.subscribe_zones    = function(cb) { this.core.moo._subscribe_helper(SVCNAME, "zones",   cb); }
+RoonApiTransport.prototype.subscribe_zones    = function(cb) {
+    this.core.moo._subscribe_helper(SVCNAME, "zones",
+                                    (response, msg) => {
+                                        if (response == "Subscribed") {
+                                            this._zones = msg.zones.reduce((p,e) => (p[e.zone_id] = e) && p, {});
+
+                                        } else if (response == "Changed") {
+                                            if (msg.zones_removed) msg.zones_removed.forEach(e => delete(this._zones[e.zone_id]));
+                                            if (msg.zones_added)   msg.zones_added  .forEach(e => this._zones[e.zone_id] = e);
+                                            if (msg.zones_changed) msg.zones_changed.forEach(e => this._zones[e.zone_id] = e);
+
+                                        } else if (response == "Unsubscribed") {
+                                            delete(this._zones);
+                                        }
+                                        cb(response, msg);
+                                    });
+}
+
+RoonApiTransport.prototype.zone_by_zone_id = function(zone_id) {
+    if (!this._zones) return null;
+    for (var x in this._zones) if (x == zone_id) return this._zones[x];
+    return null;
+}
+RoonApiTransport.prototype.zone_by_output_id = function(output_id) {
+    if (!this._zones) return null;
+    for (var x in this._zones) for (var y in this._zones[x].outputs) if (this._zones[x].outputs[y].output_id == output_id) return this._zones[x];
+    return null;
+}
+RoonApiTransport.prototype.zone_by_object = function(zone_or_output) {
+    if (zone_or_output.zone_id)   return this.zone_by_zone_id  (zone_or_output.zone_id);
+    if (zone_or_output.output_id) return this.zone_by_output_id(zone_or_output.output_id);
+    return null;
+}
 
 exports = module.exports = RoonApiTransport;
